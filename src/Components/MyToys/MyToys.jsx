b23ad7563fec001038e14ca5fc82d9b6171bffa4
SihabMolla11 <sihabuddinmolla11@gamil.com>
2023-05-20 04:37:22 +0600

    get my games

--- src/Components/MyToys/MyToys.jsx
+++ src/Components/MyToys/MyToys.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const MyToys = () => {
+
+    const {user} = useContext(AuthContext);
+    const [myGames, setMyGames] = useState([]);
+    const url =`http://localhost:5000/games/mydata?email=${user?.email}`
+    useEffect(()=>{
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=> setMyGames(data))
+    },[url]);
+
+    console.log(myGames)
+
+
     return (
         <div>
             this is my toys componets
