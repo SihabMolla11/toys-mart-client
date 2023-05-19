b03c550f4dadaab3496043b73dcc91b3c9c9407e
SihabMolla11 <sihabuddinmolla11@gamil.com>
2023-05-20 05:54:39 +0600

    my toys page design done

--- src/Components/MyToys/MyToys.jsx
+++ src/Components/MyToys/MyToys.jsx
@@ -1,25 +1,59 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
+import MyToysRwo from "./myToysRwo";
+import { FcEmptyTrash } from "react-icons/fc";
 
 const MyToys = () => {
+  const { user } = useContext(AuthContext);
+  const [myGames, setMyGames] = useState([]);
+  const url = `http://localhost:5000/games/mydata?email=${user?.email}`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMyGames(data));
+  }, [url]);
 
-    const {user} = useContext(AuthContext);
-    const [myGames, setMyGames] = useState([]);
-    const url =`http://localhost:5000/games/mydata?email=${user?.email}`
-    useEffect(()=>{
-        fetch(url)
-        .then(res=>res.json())
-        .then(data=> setMyGames(data))
-    },[url]);
+  console.log(myGames);
 
-    console.log(myGames)
+  const handelUpdateGame = (id) => {
+    console.log(id);
+  };
 
+  const handelDEleteGame = (id) => {
+    console.log(id);
+  };
 
-    return (
-        <div>
-            this is my toys componets
-        </div>
-    );
+  return (
+    <div className="my-Container">
+      <div className="overflow-x-auto">
+        <table className="table table-compact w-full">
+          <thead>
+            <tr>
+              <th>DELETE BUTTON</th>
+              <th>image</th>
+              <th>game name</th>
+              <th>sub-category</th>
+              <th>price</th>
+              <th>quantity</th>
+              <th>ratting</th>
+              <th> description</th>
+              <th>update button</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myGames.map((myGame) => (
+              <MyToysRwo
+                key={myGame._id}
+                myGame={myGame}
+                handelUpdateGame={handelUpdateGame}
+                handelDEleteGame={handelDEleteGame}
+              ></MyToysRwo>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyToys;
---
+++ src/Components/MyToys/MyToysRwo.jsx
@@ -0,0 +1,55 @@
+import { FaRegTrashAlt } from "react-icons/fa";
+
+const MyToysRwo = ({ myGame, handelDEleteGame, handelUpdateGame }) => {
+  const { _id, name, rating, description, photo, gameName, price, quantity, category } = myGame;
+
+  return (
+    <>
+      <tr>
+        <td className="text-center">
+          <button
+            onClick={() => handelDEleteGame(_id)}
+            className="text-2xl p-2 rounded-full hover:text-red-600 hover:bg-red-300 bg-red-100 text-red-500"
+          >
+            <FaRegTrashAlt />
+          </button>
+        </td>
+        <td>
+          <img
+            className="rounded-md h-20 w-20 "
+            src={photo}
+            alt="Avatar Tailwind CSS Component"
+          />
+        </td>
+        <td>
+          <p className="font-bold text-2xl">{gameName}</p>
+        </td>
+        <td>
+          <p className="font-semibold">{category.label}</p>
+        </td>
+        <td>
+          <p className="font-semibold">{"$" + price}</p>
+        </td>
+        <td>
+          <p className=" font-semibold">{quantity}</p>
+        </td>
+        <td>
+          <p className=" font-semibold">{rating}</p>
+        </td>
+        <td >
+          <p className=" font-semibold">{description}</p>
+        </td>
+        <td className="text-center">
+          <button
+            onClick={() => handelUpdateGame(_id)}
+            className="my-btn ml-4 flex items-center gap-4"
+          >
+            <span>Update</span>
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default MyToysRwo;
